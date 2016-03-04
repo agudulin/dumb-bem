8ed9c6a6186e5c61d5c7cf0a6f159163b467270a
Rafal Bromirski <rafal.bromirski@gmail.com>
2016-03-04 14:13:34 +0100

    refactoring + version update

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ const makeBlockStyles = ({
   ...props
 }) => {
   const modifiers = (block && modifier) ? modifier.split(/\s/).map((item) => (`${block}--${item}`)).join(' ') : false
+  const states = {
+    'is-active': active,
+    'is-disabled': disabled,
+    'is-hidden': hidden,
+    'is-loading': loading
+  }
 
-  return ({
-    className: cx(block, modifiers, {
-      [`${className}`]: className,
-      'is-active': active,
-      'is-disabled': disabled,
-      'is-hidden': hidden,
-      'is-loading': loading
-    }),
+  return {
+    className: cx(block, modifiers, className, states),
     ...props
-  })
+  }
 }
 
 export default (block) => ({ element, ...props }) => {
